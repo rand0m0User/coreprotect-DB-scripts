1c7caa08b43f47fa92d4bb6580bbd689b6e071b2
rand0m0User <79854039+rand0m0User@users.noreply.github.com>
2025-05-19 22:26:51 -0700

    add other scripts from the library

---
+++ distsearch.sql
@@ -0,0 +1,3 @@
+--an SQL implementation of the sign distance function for a circle, replace "1000" with your target radius
+--most tables have the same x, y, and z schema, so you can replace "co_block" with the desired table if need be
+SELECT * FROM co_block WHERE SQRT(POWER(<insert target X pos here> - x, 2) + POWER(<insert target z pos here> - z, 2)) <= 1000;
