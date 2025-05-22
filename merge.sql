947b98046af22afc30721c701f088227af7b9036
rand0m0User <79854039+rand0m0User@users.noreply.github.com>
2025-05-21 18:54:43 -0700

    Add files via upload
    this should work, but world id seems to be broken in strange way so it will always be one?

--- merge.sql
+++ merge.sql
@@ -23,7 +23,7 @@ WHERE NOT EXISTS (SELECT 1 FROM co_entity_map WHERE co_entity_map.entity = db2.c
 --merge the chat table from DB2 to this database
 INSERT INTO co_chat (time,user,wid,x,y,z,message) SELECT time, 
 	(SELECT id FROM co_user WHERE user = (SELECT user FROM co_user WHERE co_user.id = db2.co_chat.user)) as user,
-	(SELECT wid FROM co_world WHERE world = (SELECT world FROM co_world WHERE co_world.world = db2.co_world.world)) as wid,
+	(SELECT id FROM co_world WHERE world = (SELECT world FROM db2.co_world WHERE db2.co_world.id = wid)) as wid,
 	x, y, z, message
 	FROM db2.co_chat;
 	
@@ -34,21 +34,21 @@ INSERT INTO co_skull (time,owner) SELECT time, owner
 --merge the command table from DB2 to this database
 INSERT INTO co_command (time,user,wid,x,y,z,message) SELECT time, 
 	(SELECT id FROM co_user WHERE user = (SELECT user FROM co_user WHERE co_user.id = db2.co_command.user)) as user,
-	(SELECT wid FROM co_world WHERE world = (SELECT world FROM co_world WHERE co_world.world = db2.co_world.world)) as wid,
+	(SELECT id FROM co_world WHERE world = (SELECT world FROM db2.co_world WHERE db2.co_world.id = wid)) as wid,
 	x, y, z, message
 	FROM db2.co_command;
 
 --merge the sign table from DB2 to this database
 INSERT INTO co_sign (time,user,wid,x,y,z,action,color,color_secondary,data,waxed,face,line_1,line_2,line_3,line_4,line_5,line_6,line_7,line_8) SELECT time, 
 	(SELECT id FROM co_user WHERE user = (SELECT user FROM co_user WHERE co_user.id = db2.co_sign.user)) as user,
-	(SELECT wid FROM co_world WHERE world = (SELECT world FROM co_world WHERE co_world.world = db2.co_world.world)) as wid,
+	(SELECT id FROM co_world WHERE world = (SELECT world FROM db2.co_world WHERE db2.co_world.id = wid)) as wid,
 	x, y, z, action, color, color_secondary, data, waxed, face, line_1, line_2, line_3, line_4, line_5, line_6, line_7, line_8
 	FROM db2.co_sign;
 		
 --merge the item table from DB2 to this database
 INSERT INTO co_item (time,user,wid,x,y,z,type,data,amount,action,rolled_back) SELECT time, 
 	(SELECT id FROM co_user WHERE user = (SELECT user FROM co_user WHERE co_user.id = db2.co_item.user)) as user,
-	(SELECT wid FROM co_world WHERE world = (SELECT world FROM co_world WHERE co_world.world = db2.co_world.world)) as wid,
+	(SELECT id FROM co_world WHERE world = (SELECT world FROM db2.co_world WHERE db2.co_world.id = wid)) as wid,
 	x, y, z,
 	(SELECT id FROM co_material_map WHERE material = (SELECT material FROM co_material_map WHERE co_material_map.id = db2.co_item.type)) as type,
 		data, amount, action, rolled_back
@@ -58,23 +58,23 @@ INSERT INTO co_item (time,user,wid,x,y,z,type,data,amount,action,rolled_back) SE
 INSERT INTO co_entity (time,data) SELECT time, data
 	FROM db2.co_entity;
 
+--merge the container table from DB2 to this database
+INSERT INTO co_container (time,user,wid,x,y,z,type,data,amount,metadata,action,rolled_back) SELECT time, 
+	(SELECT id FROM co_user WHERE user = (SELECT user FROM co_user WHERE co_user.id = db2.co_container.user)) as user,
+	(SELECT id FROM co_world WHERE world = (SELECT world FROM db2.co_world WHERE db2.co_world.id = wid)) as wid,
+	x, y, z,
+	(SELECT id FROM co_material_map WHERE material = (SELECT material FROM co_material_map WHERE co_material_map.id = db2.co_container.type)) as type,
+		data, amount, metadata, action, rolled_back
+	FROM db2.co_container;
+
 --merge the block table from DB2 to this database
 INSERT INTO co_block (time,user,wid,x,y,z,type,data,meta,blockdata,action,rolled_back) SELECT time, 
 	(SELECT id FROM co_user WHERE user = (SELECT user FROM co_user WHERE co_user.id = db2.co_block.user)) as user,
-	(SELECT wid FROM co_world WHERE world = (SELECT world FROM co_world WHERE co_world.world = db2.co_world.world)) as wid,
+	(SELECT id FROM co_world WHERE world = (SELECT world FROM db2.co_world WHERE db2.co_world.id = wid)) as wid,
 	x, y, z,
 	(SELECT id FROM co_material_map WHERE material = (SELECT material FROM co_material_map WHERE co_material_map.id = db2.co_block.type)) as type,
 	(SELECT id FROM co_blockdata_map WHERE data = (SELECT data FROM co_blockdata_map WHERE co_blockdata_map.id = db2.co_block.data)) as type,
 		meta, blockdata, action, rolled_back
 	FROM db2.co_block;
 
---merge the container table from DB2 to this database
-INSERT INTO co_container (time,user,wid,x,y,z,type,data,amount,metadata,action,rolled_back) SELECT time, 
-	(SELECT id FROM co_user WHERE user = (SELECT user FROM co_user WHERE co_user.id = db2.co_container.user)) as user,
-	(SELECT wid FROM co_world WHERE world = (SELECT world FROM co_world WHERE co_world.world = db2.co_world.world)) as wid,
-	x, y, z,
-	(SELECT id FROM co_material_map WHERE material = (SELECT material FROM co_material_map WHERE co_material_map.id = db2.co_container.type)) as type,
-		data, amount, metadata, action, rolled_back
-	FROM db2.co_container;
-
-DETACH DATABASE db2;
+--DETACH DATABASE db2;
